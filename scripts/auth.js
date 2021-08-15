054d791b99015127c29f979af1c9f3d7243a9204
Adnan Khan <adnan7.zain@gmail.com>
2021-08-15 13:01:01 +0500

    Login or Sign In user with email and password - Authentication - firebase for web

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,6 @@
+const myModal = document.querySelectorAll('.modal');
+
+//signup
 const signup = async (e) => {
   e.preventDefault();
   const email = document.querySelector('#signupEmail');
@@ -13,4 +16,28 @@ const signup = async (e) => {
   } catch (err) {
     M.toast({ html: err.message, classes: 'red' });
   }
+
+  email.value = '';
+  password.value = '';
+  M.Modal.getInstance(myModal[0]).close();
+};
+
+//login
+const login = async (e) => {
+  e.preventDefault();
+  const email = document.querySelector('#loginEmail');
+  const password = document.querySelector('#loginPassword');
+
+  try {
+    const result = await firebase
+      .auth()
+      .signInWithEmailAndPassword(email.value, password.value);
+    M.toast({ html: `Welcom ${result.user.email}`, classes: 'green' });
+    console.log(result);
+  } catch (err) {
+    M.toast({ html: err.message, classes: 'red' });
+  }
+  email.value = '';
+  password.value = '';
+  M.Modal.getInstance(myModal[1]).close();
 };
